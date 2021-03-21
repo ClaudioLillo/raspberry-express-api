34d31ee848176b718e9bddedf6f5e0103ef2e321
Claudio Lillo <cilillo@uc.cl>
2021-03-21 09:37:53 -0300

    app use multer to upload files

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 import express, {json} from 'express'
+import multer from 'multer'
+import morgan from 'morgan'
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+   
+  const upload = multer({ storage: storage })
 
 const app = express()
 app.use(json())
+app.use(morgan('combined'))
 
 app.get('/', (req, res)=>{
     return res.status(200).json({
         author: "Claudio Lillo",
         version: "1.0"
     })
+})
+app.post('/upload', upload.array('file',5), (req, res, next)=>{
+    const files = req.files
+    if(!files){
+        const error = new Error('Please choose files')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    return res.status(200).json({files: files})
 })
 
 export default app
