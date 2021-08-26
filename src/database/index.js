b6304d2217bbb4b269057c825fc4daf98459b19b
Claudio Lillo <cilillo@uc.cl>
2021-08-26 12:59:44 -0400

    modified port

--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,9 @@ import mariadb from 'mariadb'
 const database=process.env.DATABASE
 const username=process.env.USER
 const password=process.env.PASSWORD
+const port=process.env.PORT || '3306'
 
-const sequelize = new Sequelize(`mariadb://${username}:${password}@localhost:3308/${database}`)
+const sequelize = new Sequelize(`mariadb://${username}:${password}@localhost:${port}/${database}`)
 
 
 module.exports = sequelize
