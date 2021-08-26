89fdf018c2682f52d46089d3b45e19d7c61e66ee
Claudio Lillo <cilillo@uc.cl>
2021-08-26 13:02:05 -0400

    change pass

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,9 @@ import {Sequelize} from 'sequelize'
 import mariadb from 'mariadb'
 
 
-const database=process.env.DATABASE
-const username=process.env.USER
-const password=process.env.PASSWORD
+const database=process.env.DATABASE || "elm"
+const username=process.env.USER || "root"
+const password=process.env.PASSWORD ||"triptofano"
 const port=process.env.PORT || '3306'
 
 const sequelize = new Sequelize(`mariadb://${username}:${password}@localhost:${port}/${database}`)
